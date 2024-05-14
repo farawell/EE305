2cc1f222f0796cd9cbb5d10f6f56fa551f1ac957
John Park <john7park@naver.com>
2024-05-15 05:27:06 +0900

    Edited func_QPSK_modulation.m

--- EE305_Exp7/func_QPSK_modulation.m
+++ EE305_Exp7/func_QPSK_modulation.m
@@ -2,9 +2,9 @@ function symbols_packet_tx = func_QPSK_modulation(encoded_bits_packet_tx)
 
     j=sqrt(-1);
     number_of_bits_per_symbol = 2;
-    len= length(encoded_bits_packet_tx)/number_of_bits_per_symbol;
+    len= length(encoded_bits_packet_tx) / number_of_bits_per_symbol;
     
-    symbols_packet_tx = zeros(len,1);
+    symbols_packet_tx = zeros(len, 1);
     
     b00 = [0; 0];
     b01 = [0; 1];
@@ -16,18 +16,22 @@ function symbols_packet_tx = func_QPSK_modulation(encoded_bits_packet_tx)
     s10 = (-1+j)/sqrt(2);
     s11 = (-1-j)/sqrt(2);    
     
-    for ind=1:len
+    for ind = 1 : len
     %=======================================================================  
     % Perform QPSK modulation (cf. PCM)
-        bits = encoded_bits_packet_tx((ind-1)*number_of_bits_per_symbol + 1 : ind * number_of_bits_per_symbol);
-        if bits == b00
-            symbols_packet_tx(ind) = s00;
-        elseif bits == b01
-            symbols_packet_tx(ind) = s01;
-        elseif bits == b10
-            symbols_packet_tx(ind) = s10;
-        elseif bits == b11
-            symbols_packet_tx(ind) = s11;
+        % Adding a mapping from bit patterns to QPSK symbols
+        bit_patterns = [b00 b01 b10 b11];
+        symbols = [s00 s01 s10 s11];    
+
+        sym_idx_start = (ind - 1) * number_of_bits_per_symbol + 1;
+        sym_idx_end = ind * number_of_bits_per_symbol;
+        bits = encoded_bits_packet_tx(sym_idx_start : sym_idx_end);
+
+        for k = 1 : 4
+            if isequal(bits, bit_patterns(:, k))
+                symbols_packet_tx(ind) = symbols(k);
+                break;
+            end
         end
     %=======================================================================    
     end
