df3e50a7e873ad19fc5e8abbbbdd6871ac41767a
jaewoo <bugiljaewoo@gmail.com>
2024-05-15 04:55:41 +0900

    Added some comments of func_PCM_coding.m

--- EE305_Exp7/func_PCM_coding.m
+++ EE305_Exp7/func_PCM_coding.m
@@ -2,7 +2,9 @@ function bit_stream_tx = func_PCM_coding(data, bitsPerSample)
 
     bit_stream_tx = zeros(length(data)*bitsPerSample,1);
     bits = zeros(bitsPerSample,1);
-    
+
+
+    % Convert the data to digital signal
     for ind = 1:length(data)
         value = data(ind);  % Each data value ranges from -1 to 1
         value = (2^(bitsPerSample-1)-0.5) * value; % scaling
@@ -13,7 +15,7 @@ function bit_stream_tx = func_PCM_coding(data, bitsPerSample)
         % Round down the variable 'value'
         flr = floor(value);
 
-        % When 'flr' is negative
+        % When 'flr' is negative (2's complement)
         if (flr < 0)
             flr = flr + 2^(bitsPerSample);
         end
@@ -22,6 +24,7 @@ function bit_stream_tx = func_PCM_coding(data, bitsPerSample)
         bits = de2bi(flr, bitsPerSample, 'left-msb');
         %=======================================================================
         
+        % Save the converted binary number in bit_stream_tx array
         bit_stream_tx( (ind-1)* bitsPerSample +1: ind* bitsPerSample ) = bits;
        
     end
