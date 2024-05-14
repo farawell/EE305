2a877feefb40f748a286d6ee03bab733c9bbaed6
John Park <john7park@naver.com>
2024-05-15 05:36:30 +0900

    Unified the spacing format and added minor comments to func_QPSK_modulation.m

--- EE305_Exp7/func_QPSK_modulation.m
+++ EE305_Exp7/func_QPSK_modulation.m
@@ -1,20 +1,20 @@
 function symbols_packet_tx = func_QPSK_modulation(encoded_bits_packet_tx)
 
-    j=sqrt(-1);
+    j = sqrt(-1);
     number_of_bits_per_symbol = 2;
-    len= length(encoded_bits_packet_tx) / number_of_bits_per_symbol;
+    len = length(encoded_bits_packet_tx) / number_of_bits_per_symbol;
     
-    symbols_packet_tx = zeros(len, 1);
+    symbols_packet_tx = zeros(len, 1); % Initialization
     
     b00 = [0; 0];
     b01 = [0; 1];
     b10 = [1; 0];
     b11 = [1; 1];
     
-    s00 = (1+j)/sqrt(2);
-    s01 = (1-j)/sqrt(2);
-    s10 = (-1+j)/sqrt(2);
-    s11 = (-1-j)/sqrt(2);    
+    s00 = (1+j) / sqrt(2);
+    s01 = (1-j) / sqrt(2);
+    s10 = (-1+j) / sqrt(2);
+    s11 = (-1-j) / sqrt(2);    
     
     for ind = 1 : len
     %=======================================================================  
